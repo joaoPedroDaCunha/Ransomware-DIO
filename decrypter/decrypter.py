f4b4663451a804f6a762a647cafd7711c2075c03
joaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-12-23 17:47:42 -0300

    encrypter

--- decrypter/decrypter.py
+++ decrypter/decrypter.py
@@ -20,6 +20,6 @@ os.remove(file_name)
 ## criar um novo arquivo descriptogarfado 
 
 new_file = 'test.txt'
-new_file = open(f'{new_file}',)
+new_file = open(f'{new_file}','wb')
 new_file.write(decrypt_data)
 new_file.close()
